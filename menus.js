ee266c9743f5798f5fc85d836269916e41c56e6a
Roquez <roquez94@gmail.com>
2023-03-09 22:58:31 -0500

    Finished module

--- menus.js
+++ menus.js
@@ -1,5 +1,20 @@
-function dropDownMenu () {
-    let menu = document.getElementsByClassName(menu);
+//When user clicks on the button, toggle between hiding
+//and showing the dropdown content links
+function downButton() {
+    document.getElementById("myDropdown").classList.toggle('show');
+}
 
+//Close the dropdown menu if the user clicks outside of it
+window.onclick = function (e){
+    if (!e.target.matches('.dropbtn')) {
+        let dropdowns = document.getElementsByClassName('dropDownLinks');
+
+        for (let i =0; i < dropdowns.length; i++) {
+            let openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')){
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
 
 }
